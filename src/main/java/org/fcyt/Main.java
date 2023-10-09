206aef1ed4312e78b3cfb4886acb627751b6e0d7
Cristhian Mendieta <cmendieta2311@gmail.com>
2023-10-09 17:44:28 -0300

    Se agrega ventana principal

--- src/main/java/org/fcyt/Main.java
+++ src/main/java/org/fcyt/Main.java
@@ -1,23 +1,26 @@
 package org.fcyt;
-
-import org.fcyt.modelo.Conexion;
-import org.fcyt.modelo.Empresa;
 import org.fcyt.modelo.dao.EmpresaDaoImpl;
-
-import java.sql.Connection;
-import java.util.List;
 import org.fcyt.controlador.EmpresaController;
+import org.fcyt.controlador.IvaController;
+import org.fcyt.controlador.MarcaController;
+import org.fcyt.modelo.dao.IvaDaoImpl;
+import org.fcyt.modelo.dao.MarcaDaoImpl;
 import org.fcyt.vista.GUIEmpresa;
+import org.fcyt.vista.GUIIva;
+import org.fcyt.vista.GUIMarca;
+import org.fcyt.vista.GUIVentanaPrincipal;
 
+/**
+ * Este metodo se encarga de iniciar la ejecucion del programa
+ * Este es el metodo principal del proyecto
+ * @param args[] es un arreglo con los parametros que el reciba por consola
+ * @return void
+ **/
 public class Main {
     public static void main(String[] args) {
-//        Conexion conectar = new Conexion();
-//        Connection conec = conectar.conectarBD();
-
-        EmpresaDaoImpl dao = new EmpresaDaoImpl();
-        GUIEmpresa gui = new GUIEmpresa(null, true);
-        EmpresaController ctrl = new EmpresaController(gui, dao);
-        ctrl.mostrarVentana();
+        GUIVentanaPrincipal gui = new GUIVentanaPrincipal();
+        gui.setVisible(true);
+        
        
     }
 }
